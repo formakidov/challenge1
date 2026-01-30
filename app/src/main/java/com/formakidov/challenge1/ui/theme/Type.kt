f1086f30078b088f6974e8c6a81b8a920108ecd3
formakidov <formakidov1996@gmail.com>
2026-01-30 10:23:20 +0100

    Update typography styles

--- app/src/main/java/com/formakidov/challenge1/ui/theme/Type.kt
+++ app/src/main/java/com/formakidov/challenge1/ui/theme/Type.kt
@@ -1,34 +1,54 @@
 package com.formakidov.challenge1.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(700),
+        fontSize = 34.sp,
+        lineHeight = 41.sp,
+        letterSpacing = 0.4.sp,
+        color = Color(0xFF000000)
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(400),
+        fontSize = 24.sp,
+        lineHeight = 22.sp,
+        color = Color(0xFF000000)
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight(700),
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        color = Color(0xFF000000)
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp,
+        color = Color(0x993C3C43)
+    ),
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontWeight = FontWeight(400),
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        color = Color(0x993C3C43)
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight(600),
+        fontSize = 13.sp,
+        color = Color(0xFF414B57)
     )
-    */
 )
